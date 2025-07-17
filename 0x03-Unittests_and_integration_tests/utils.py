2c149b2065cb7221530ba580b21bff2d77edc304
Alvin <162141395+alvo-254@users.noreply.github.com>
2025-07-17 13:59:22 +0300

    Delete 0x03-Unittests_and_integration_tests/utils.py

--- 0x03-Unittests_and_integration_tests/utils.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-"""Utilities module"""
-
-
-def access_nested_map(nested_map, path):
-    """Access a nested map using a sequence of keys"""
-    for key in path:
-        nested_map = nested_map[key]
-    return nested_map
