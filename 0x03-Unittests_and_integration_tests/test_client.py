2ef26dd29c7d5cb9432db2d4fc0d79661885f4cb
Alvin <162141395+alvo-254@users.noreply.github.com>
2025-07-18 11:49:16 +0300

    Update test_client.py

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,21 +1,22 @@
 #!/usr/bin/env python3
-"""Unittest for GithubOrgClient"""
+"""Unit tests for client.py"""
+
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, PropertyMock
 from parameterized import parameterized
 from client import GithubOrgClient
 
 
 class TestGithubOrgClient(unittest.TestCase):
-    """Tests for GithubOrgClient"""
+    """Unit tests for GithubOrgClient"""
 
     @parameterized.expand([
         ("google", {"login": "google"}),
-        ("abc", {"login": "abc"}),
+        ("abc", {"login": "abc"})
     ])
-    @patch('client.get_json')
+    @patch("client.get_json")
     def test_org(self, org_name, expected_payload, mock_get_json):
-        """Test that GithubOrgClient.org returns expected result"""
+        """Test GithubOrgClient.org returns correct payload"""
         mock_get_json.return_value = expected_payload
         client = GithubOrgClient(org_name)
         self.assertEqual(client.org, expected_payload)
