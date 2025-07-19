4d6f416b1554d25e2d53b9a07495b8886797c214
manyaru <alvinmureithi1@gmail.com>
2025-07-19 20:13:15 +0300

    task

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -27,8 +27,13 @@ class TestGithubOrgClient(unittest.TestCase):
 
     def test_public_repos_url(self):
         """Test _public_repos_url returns the expected URL."""
-        test_payload = {"repos_url": "https://api.github.com/orgs/test_org/repos"}
-        with patch('client.GithubOrgClient.org', new_callable=PropertyMock) as mock_org:
+        test_payload = {
+            "repos_url": "https://api.github.com/orgs/test_org/repos"
+        }
+        with patch(
+            'client.GithubOrgClient.org',
+            new_callable=PropertyMock
+        ) as mock_org:
             mock_org.return_value = test_payload
             client = GithubOrgClient("test_org")
             self.assertEqual(
@@ -43,7 +48,10 @@ class TestGithubOrgClient(unittest.TestCase):
         test_repos_url = "https://api.github.com/orgs/test_org/repos"
         test_payload = [{"name": "repo1"}, {"name": "repo2"}]
         mock_get_json.return_value = test_payload
-        with patch('client.GithubOrgClient._public_repos_url', new_callable=PropertyMock) as mock_public_repos_url:
+        with patch(
+            'client.GithubOrgClient._public_repos_url',
+            new_callable=PropertyMock
+        ) as mock_public_repos_url:
             mock_public_repos_url.return_value = test_repos_url
             client = GithubOrgClient("test_org")
             self.assertEqual(client.public_repos, ["repo1", "repo2"])
