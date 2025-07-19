6a124a601f6c605cbef696c0f1bb93a151e7b3cb
manyaru <alvinmureithi1@gmail.com>
2025-07-19 20:03:47 +0300

    changes

--- 0x03-Unittests_and_integration_tests/utils.py
+++ 0x03-Unittests_and_integration_tests/utils.py
@@ -20,3 +20,10 @@ def memoize(method):
             setattr(self, attr_name, method(self))
         return getattr(self, attr_name)
     return wrapper
+
+
+def access_nested_map(nested_map, path):
+    """Access a value in a nested map with a list of keys (path)."""
+    for key in path:
+        nested_map = nested_map[key]
+    return nested_map
