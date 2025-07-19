f16083c92f0c307df76a3ad02da29f3f3ca04eb1
manyaru <alvinmureithi1@gmail.com>
2025-07-19 20:36:36 +0300

    task 8

--- 0x03-Unittests_and_integration_tests/client.py
+++ 0x03-Unittests_and_integration_tests/client.py
@@ -18,7 +18,21 @@ class GithubOrgClient:
         """Get repos_url from org payload"""
         return self.org["repos_url"]
 
-    def public_repos(self):
-        """Return list of public repo names"""
+    def public_repos(self, license=None):
+        """Return list of public repo names, optionally filtered by license"""
         repos = get_json(self._public_repos_url)
-        return [repo["name"] for repo in repos]
+        if license is None:
+            return [repo["name"] for repo in repos]
+        return [
+            repo["name"]
+            for repo in repos
+            if self.has_license(repo, license)
+        ]
+
+    @staticmethod
+    def has_license(repo, license_key):
+        """Check if a repository has a specific license"""
+        try:
+            return repo["license"]["key"] == license_key
+        except (KeyError, TypeError):
+            return False
