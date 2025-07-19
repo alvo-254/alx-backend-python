c3dfb94be66090ec3fc411f08ce0de2502d38961
manyaru <alvinmureithi1@gmail.com>
2025-07-19 20:15:56 +0300

    task 6 client.py

--- 0x03-Unittests_and_integration_tests/client.py
+++ 0x03-Unittests_and_integration_tests/client.py
@@ -1,6 +1,7 @@
 from utils import get_json
 from utils import memoize
 
+
 class GithubOrgClient:
     """GitHub Org client"""
 
@@ -9,4 +10,15 @@ class GithubOrgClient:
 
     @memoize
     def org(self):
+        """Get org details from GitHub API"""
         return get_json(f"https://api.github.com/orgs/{self.org_name}")
+
+    @property
+    def _public_repos_url(self):
+        """Get repos_url from org payload"""
+        return self.org["repos_url"]
+
+    def public_repos(self):
+        """Return list of public repo names"""
+        repos = get_json(self._public_repos_url)
+        return [repo["name"] for repo in repos]
