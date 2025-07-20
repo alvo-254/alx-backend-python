38d1d87fb290bb12228e58242ef0dec35ed12c7d
manyaru <alvinmureithi1@gmail.com>
2025-07-20 18:08:34 +0300

    comit task 9

--- 0x03-Unittests_and_integration_tests/fixtures.py
+++ 0x03-Unittests_and_integration_tests/fixtures.py
@@ -1,16 +1,16 @@
-#!/usr/bin/env python3
-"""Fixtures for GithubOrgClient integration tests."""
-
 org_payload = {
+    "login": "google",
+    "id": 1342004,
     "repos_url": "https://api.github.com/orgs/google/repos"
 }
 
 repos_payload = [
-    {"name": "repo1", "license": {"key": "apache-2.0"}},
-    {"name": "repo2", "license": {"key": "mit"}},
-    {"name": "repo3", "license": {"key": "gpl-3.0"}}
+    {"id": 1, "name": "repo1", "license": {"key": "apache-2.0"}},
+    {"id": 2, "name": "repo2", "license": {"key": "other"}},
+    {"id": 3, "name": "repo3", "license": {"key": "apache-2.0"}},
+    {"id": 4, "name": "repo4", "license": None}
 ]
 
-expected_repos = ["repo1", "repo2", "repo3"]
+expected_repos = ["repo1", "repo2", "repo3", "repo4"]
 
-apache2_repos = ["repo1"]
+apache2_repos = ["repo1", "repo3"]
