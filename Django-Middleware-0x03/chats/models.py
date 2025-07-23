b7d97ff300a9be133d69e80cb93f38919d2c9344
manyaru <alvinmureithi1@gmail.com>
2025-07-23 12:00:11 +0300

    task 4

--- Django-Middleware-0x03/chats/models.py
+++ Django-Middleware-0x03/chats/models.py
@@ -4,20 +4,27 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
+    ROLE_CHOICES = (
+        ('user', 'User'),
+        ('moderator', 'Moderator'),
+        ('admin', 'Admin'),
+    )
+
     user_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True, primary_key=True)
     phone_number = models.CharField(max_length=15, unique=True)
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=128)
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='user')
 
     def __str__(self):
-        return f"{self.username} ({self.phone_number})"
+        return f"{self.username} ({self.role})"
 
 
 class Conversation(models.Model):
     conversation_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True, primary_key=True)
-    participants = models.ManyToManyField(User, related_name='conversations')
+    participants = models.ManyToManyField("User", related_name='conversations')
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
@@ -26,8 +33,8 @@ class Conversation(models.Model):
 
 class Message(models.Model):
     message_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True, primary_key=True)
-    conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE, related_name='messages')
-    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='messages')
+    conversation = models.ForeignKey("Conversation", on_delete=models.CASCADE, related_name='messages')
+    sender = models.ForeignKey("User", on_delete=models.CASCADE, related_name='messages')
     message_body = models.TextField()
     sent_at = models.DateTimeField(auto_now_add=True)
 
